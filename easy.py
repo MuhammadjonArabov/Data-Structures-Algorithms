59f126c03c356b229af81445c18815e45de6d169
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2024-10-30 18:29:48 +0500

    update

--- easy.py
+++ easy.py
@@ -4,14 +4,16 @@ You may assume that each input would have exactly one solution, and you may not 
 
 You can return the answer in any order. '''
 
-def two(nums, target):
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            print(nums[i], nums[j])
-            if nums[i] + nums[j] == target:
-                return [i, j]
-
-print(two([23, 2, 3, 4, 5, 5], 9))
-
+class Solution():
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                print(nums[i], nums[j])
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 
 
+solution = Solution()
+print(solution.twoSum([1, 2, 3, 4, 5, 6], 9))
+print(solution.twoSum([11, 2, 3, 41, 52, 63], 5))
+print(solution.twoSum([13, 25, 39, 40, 55, 6], 19))
