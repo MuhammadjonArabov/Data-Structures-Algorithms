8b73f75357025ac68d0ab9bbec2d71fd97f47e51
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2024-11-02 22:44:16 +0500

    romanToInt

--- easy.py
+++ easy.py
@@ -8,7 +8,6 @@ class Solution():
     def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
-                print(nums[i], nums[j])
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
@@ -20,9 +19,9 @@ print(solution.twoSum([13, 25, 39, 40, 55, 6], 19))
 
 # ======================================================================================================================
 
-'''2. Given an integer x, return true if x is a palindrome and false otherwise.'''
+'''9. Given an integer x, return true if x is a palindrome and false otherwise.'''
 
-class Solution1():
+class Solution9():
     def isPalindrome(self, x):
         if x < 0:
             return False
@@ -30,7 +29,57 @@ class Solution1():
         return str_x == str_x[::-1]
 
 
-solution1 = Solution1()
-print(solution1.isPalindrome(121))
-print(solution1.isPalindrome(12))
-print(solution1.isPalindrome(1221))
+solution9 = Solution9()
+print(solution9.isPalindrome(121))
+print(solution9.isPalindrome(12))
+print(solution9.isPalindrome(1221))
+
+
+# ======================================================================================================================
+
+'''
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+'''
+
+class Solution13():
+    def romanToInt(self, s):
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        total = 0
+        prev_value = 0
+        for char in reversed(s):
+            value = roman_values[char]
+            print(value)
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+            prev_value = value
+        return total
+
+solution13 = Solution13()
+print(solution13.romanToInt('IV'))
