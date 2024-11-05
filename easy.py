a0eb3aff2ddbba49b716e1b3c3e52a68209b7563
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2024-11-05 20:44:48 +0500

    longestCommonPrefix

--- easy.py
+++ easy.py
@@ -4,6 +4,7 @@ You may assume that each input would have exactly one solution, and you may not 
 
 You can return the answer in any order. '''
 
+
 class Solution():
     def twoSum(self, nums, target):
         for i in range(len(nums)):
@@ -81,4 +82,30 @@ class Solution13():
         return total
 
 solution13 = Solution13()
-print(solution13.romanToInt('VI'))
+print(solution13.romanToInt('IV'))
+
+#=======================================================================================================================
+
+'''14. Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".'''
+
+
+class Solution14():
+    def longestCommonPrefix(self, strs):
+      if not strs:
+          return ""
+
+      prefix = strs[0]
+
+      for i in strs[1:]:
+          while not i.startswith(prefix):
+              prefix = prefix[:-1]
+              if not prefix:
+                  return ""
+      return prefix
+
+solution14 = Solution14()
+print(solution14.longestCommonPrefix(['olma', 'olcha', 'olxora']))
+print(solution14.longestCommonPrefix(['olma', 'olcha', 'shaftoli']))
+
