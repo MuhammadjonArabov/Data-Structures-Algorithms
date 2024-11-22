f53a21d875d7a740a25bd8ba0060f721ae34397d
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2024-11-22 19:34:56 +0500

    removeElement

--- easy.py
+++ easy.py
@@ -229,3 +229,30 @@ class Solution26():
 solution26 = Solution26()
 print(solution26.removeDuplicates([1, 1, 2]))
 print(solution26.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+
+#=======================================================================================================================
+
+'''27. Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the 
+elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need 
+to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+The remaining elements of nums are not important as well as the size of nums.
+Return k'''
+
+class Solution27(object):
+    def removeElement(self, nums, val):
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
+solution27 = Solution27()
+nums = [3, 2, 2, 3]
+k = solution27.removeElement(nums, 3)
+print(k, ',', nums[:k])
+
