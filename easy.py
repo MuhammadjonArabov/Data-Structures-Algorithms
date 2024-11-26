bcbd120339c9bc2a04475d256e151443c159539a
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2024-11-26 18:01:21 +0500

    plusOne

--- easy.py
+++ easy.py
@@ -3,6 +3,9 @@
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 You can return the answer in any order. '''
+from string import digits
+
+from django.db.models.fields import return_None
 
 
 class Solution(object):
@@ -325,4 +328,28 @@ class Solution58(object):
 solution58 = Solution58()
 print(solution58.lengthOfLastWord("Hello World"))
 print(solution58.lengthOfLastWord("fly me   to   the moon"))
+
+#=======================================================================================================================
+
+'''66. You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the 
+integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer
+ does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.'''
+
+class Solution66(object):
+    def plusOne(self, digits):
+       for i in range(len(digits) - 1, -1, -1):
+           if digits[i] < 9:
+               digits[i] += 1
+               return digits
+           digits[i] = 0
+       return [1] + digits
+
+solution66 = Solution66()
+print(solution66.plusOne([1, 2, 3, 4]))
+print(solution66.plusOne([3, 2, 3, 9]))
+
+#=======================================================================================================================
+
 
