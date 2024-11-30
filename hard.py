85397253af3a81ab0b1683bd607dadcf1279a883
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2024-11-30 21:56:41 +0500

    findMedianSortedArrays

--- hard.py
+++ hard.py
@@ -18,7 +18,31 @@ class Solution10(object):
 
         return match(0, 0)
 
+
 solution10 = Solution10()
 print(solution10.isMatch("aa", "a*"))  # True
 print(solution10.isMatch("mississippi", "mis*is*p*."))  # False
 print(solution10.isMatch("ab", ".*"))  # True
+
+# =======================================================================================================================
+
+'''4. Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n)).'''
+
+from typing import List
+
+
+class Solution4:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        m = nums1 + nums2
+        m.sort()
+        n = len(m)
+
+        if n % 2 == 1:
+            return m[n // 2]
+        else:
+            return (m[n // 2] + m[n // 2 - 1]) / 2
+
+
+solution4 = Solution4()
+print(solution4.findMedianSortedArrays([22, 23, 24], [25, 26, 27, 28, 29]))
