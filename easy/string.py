5bb5bce88548cb2d44d910a0e8c17e0da35f9e94
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2024-12-20 20:47:46 +0500

    romanToInto

--- easy/string.py
+++ easy/string.py
@@ -24,27 +24,3 @@ solution13 = Solution13()
 print(solution13.romanToInto('IV'))
 print(solution13.romanToInto('VII'))
 
-'''
-14. Write a function to find the longest common prefix string amongst an array of strings.
-If there is no common prefix, return an empty string "".
-'''
-
-
-class Solution14:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
-
-        prefix = strs[0]
-
-        for string in strs[1:]:
-            while not string.startswith(prefix):
-                prefix = prefix[:-1]
-                if not prefix:
-                    return ""
-
-        return prefix
-
-
-solution14 = Solution14()
-print(solution14.longestCommonPrefix(["flower", "flow", "flight"]))
