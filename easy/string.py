3cf1803a103763caed5d2beca0673435da943a9d
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2024-12-20 18:53:37 +0500

    romanToInto

---
+++ easy/string.py
@@ -0,0 +1,24 @@
+'''
+13. Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+'''
+
+
+class Solution13:
+    def romanToInto(self, s: str) -> int:
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev_value = 0
+        for char in s:
+            current_value = roman_map[char]
+            if current_value > prev_value:
+                print(current_value, 888)
+                total += current_value - 2 * prev_value
+            else:
+                total += current_value
+            prev_value = current_value
+        return total
+
+
+solution13 = Solution13()
+print(solution13.romanToInto('IV'))
+print(solution13.romanToInto('VII'))
--- medium/medium.py
+++ medium/medium.py
@@ -1,3 +1,6 @@
+from builtins import str
+
+
 class Solution8(object):
     def myAtoi(self, s: str):
         s = s.lstrip()
@@ -27,3 +30,32 @@ class Solution8(object):
 
 solution8 = Solution8()
 print(solution8.myAtoi('   -042'))
+
+#=======================================================================================================================
+
+'''You are given a string s that consists of lowercase English letters.
+A string is called special if it is made up of only a single character. For example, the string "abc" is not special,
+ whereas the strings "ddd", "zz", and "f" are special.'''
+
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        n = len(s)
+        max_length = -1
+
+        for length in range(1, n + 1):
+            freq = {}
+            for i in range(n - length + 1):
+                substring = s[i:i + length]
+                if len(set(substring)) == 1:
+                    freq[substring] = freq.get(substring, 0) + 1
+
+
+            for substring, count in freq.items():
+                if count >= 3:
+                    max_length = max(max_length, len(substring))
+
+        return max_length
+
+
+str = 'salom'
+print(str.encode('utf-8'))
