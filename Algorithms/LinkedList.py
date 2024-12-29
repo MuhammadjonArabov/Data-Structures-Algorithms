16be5df4e76deddc4bfe570ed70ec9fbf99b2184
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2024-12-29 19:02:34 +0500

    update

--- Algorithms/LinkedList.py
+++ Algorithms/LinkedList.py
@@ -1,6 +1,6 @@
 class Node:
     def __init__(self, data):
-        self.data = data,
+        self.data = data
         self.next = None
 
 
@@ -10,50 +10,58 @@ class LinkedList:
 
     def printList(self):
         '''Malumotlarni chop qilish'''
+        if self.head is None:
+            print("Ro'yxat bo'sh")
+            return
         temp = self.head
         while temp:
             print(temp.data)
             temp = temp.next
 
     def push(self, new_data):
-        '''Boshiga elemnt qo'shish'''
-        new_data = Node(new_data)
-        new_data.next = self.head
-        self.head = new_data
+        '''Boshiga element qo'shish'''
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
 
     def insertAfter(self, prev_node, new_data):
-        '''Birir tugundan keyin element qo'shihs'''
-        if prev_node is Node:
+        '''Biror tugundan keyin element qo'shish'''
+        if prev_node is None:
             print("Tugun mavjud emas")
-        new_data = Node(new_data)
-        new_data.next = prev_node.next
-        prev_node.next = new_data
+            return
+        new_node = Node(new_data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
 
     def append(self, new_data):
-        '''Oxriga element qo'shish'''
-        new_data = Node(new_data)
+        '''Oxiriga element qo'shish'''
+        new_node = Node(new_data)
         if self.head is None:
-            self.head = new_data
+            self.head = new_node
             return
         last = self.head
         while last.next:
             last = last.next
-        last.next = new_data
+        last.next = new_node
 
     def deleteNode(self, key):
         '''Listni qiymatini o'chirish'''
         temp = self.head
+
         if temp and temp.data == key:
             self.head = temp.next
             temp = None
             return
 
+        prev = None
         while temp:
             if temp.data == key:
                 break
             prev = temp
             temp = temp.next
-        if temp == None:
+
+        if temp is None:
+            print(f"'{key}' qiymati ro'yxatda topilmadi")
             return
 
         prev.next = temp.next
@@ -68,6 +76,6 @@ llist.head.next = tusdey
 tusdey.next = wansdey
 llist.push("Yakshanba")
 llist.insertAfter(llist.head.next, "Salom Dushanba")
-#llist.append("Payshanba")
+# llist.append("Payshanba")
 llist.deleteNode("Payshanba")
 print(llist.printList())
