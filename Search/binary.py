b760b22d9568c33e7e6b19975d3004174ff144c8
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-01-01 16:37:20 +0500

    binary search

---
+++ Search/binary.py
@@ -0,0 +1,24 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        elif arr[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return -1
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 5
+result = binary_search(arr, target)
+
+if result != -1:
+    print(f"Element {target} index topildi: {result}")
+else:
+    print(f"Element {target} topilmadi")
