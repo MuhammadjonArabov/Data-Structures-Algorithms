63170c788fc4fb8beb8f13e432fc8abef075109a
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-01-01 17:11:13 +0500

    binary search

--- Search/binary.py
+++ Search/binary.py
@@ -6,16 +6,15 @@ def binary_search(arr, target):
 
         if arr[mid] == target:
             return mid
-
         elif arr[mid] > target:
             right = mid - 1
         else:
             left = mid + 1
-
     return -1
 
+
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-target = 5
+target = 4
 result = binary_search(arr, target)
 
 if result != -1:
